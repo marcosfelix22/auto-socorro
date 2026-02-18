aa903bd3ef4cf50c041ba858a28022f77c517def
marcosfelix22 <marcosfelix22@gmail.com>
2026-02-18 14:34:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
+const VERSION = 'v2'; // Mudar isso força o Chrome a atualizar
 self.addEventListener('install', (e) => {
   self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request));
